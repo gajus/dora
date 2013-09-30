00cfcc372f3b01046b21db66cd584c3871629aeb
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-09-30 06:25:36 +0000

    Implemented flash memory.

--- form.php
+++ form.php
@@ -6,27 +6,31 @@ class Form {
 		$uid,
 		$uid_index = 0,
 		$data = [],
-		$input_index = [];
+		$input_index = [],
+		$is_submitted = false;
 	
 	public function __construct (array $data = null) {
 		$this->data = $data === null ? [] : $data;
 		
 		$caller = debug_backtrace()[0]; // Where was __toString triggered?
 		
-		$this->uid = 'thorax-' . crc32($caller['file'] . '_' . $caller['line']);
+		$this->uid = crc32($caller['file'] . '_' . $caller['line']);
 		
-		if (isset($_POST['thorax']['uid']) && $_POST['thorax']['uid'] === $this->uid) {
-			unset($_POST['thorax']);
-			
-			$this->data = array_merge_recursive($this->data, $_POST);
-		} else if (isset($_GET['thorax']['uid']) && $_GET['thorax']['uid'] === $this->uid) {
-			unset($_GET['thorax']);
-			
-			$this->data = array_merge_recursive($this->data, $_GET);
+		if (isset($_SESSION['thorax']['flash'][$this->uid]['input'])) {
+			$this->data = $_SESSION['thorax']['flash'][$this->uid]['input'];
 		}
 		
-		// Prevent accidental output buferring.
-		ob_start();
+		if (isset($_POST['thorax']['uid']) && $_POST['thorax']['uid'] == $this->uid) {
+			unset($_POST['thorax']);
+			
+			$this->data = array_merge_recursive_distinct($this->data, $_POST);
+			
+			$this->is_submitted = true;
+			
+			$_SESSION['thorax']['flash'][$this->uid]['input'] = $this->data;
+		} else {
+			unset($_SESSION['thorax']['flash']);
+		}
 	}
 	
 	public function input ($name, array $attributes = null, array $parameters = null) {
@@ -41,6 +45,12 @@ class Form {
 		return $input;
 	}
 	
+	public function is ($event) {
+		if ($event === 'submitted') {
+			return $this->is_submitted;
+		}
+	}
+	
 	public function getData () {
 		return $this->data;
 	}
@@ -52,4 +62,47 @@ class Form {
 	public function getUidIndex () {
 		return $this->uid_index++;
 	}
+	
+	public function clearFlash () {
+		unset($_SESSION['thorax']['flash'][$this->uid]['input']);
+	}
+}
+
+/**
+ * array_merge_recursive does indeed merge arrays, but it converts values with duplicate
+ * keys to arrays rather than overwriting the value in the first array with the duplicate
+ * value in the second array, as array_merge does. I.e., with array_merge_recursive,
+ * this happens (documented behavior):
+ *
+ * array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
+ *     => array('key' => array('org value', 'new value'));
+ *
+ * array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
+ * Matching keys' values in the second array overwrite those in the first array, as is the
+ * case with array_merge, i.e.:
+ *
+ * array_merge_recursive_distinct(array('key' => 'org value'), array('key' => 'new value'));
+ *     => array('key' => array('new value'));
+ *
+ * Parameters are passed by reference, though only for performance reasons. They're not
+ * altered by this function.
+ *
+ * @param array $array1
+ * @param array $array2
+ * @return array
+ * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
+ * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
+ */
+function array_merge_recursive_distinct ( array &$array1, array &$array2 )	{
+	$merged = $array1;
+	
+	foreach ( $array2 as $key => &$value ) {
+		if ( is_array ( $value ) && isset ( $merged [$key] ) && is_array ( $merged [$key] ) ) {
+			$merged [$key] = array_merge_recursive_distinct ( $merged [$key], $value );
+		} else {
+			$merged [$key] = $value;
+		}
+	}
+	
+	return $merged;
 }
