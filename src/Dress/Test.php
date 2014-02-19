2692bdebcb45f1ca6439b58c7f60ff2a5bb806b2
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-02-19 04:25:24 +0000

    Package header.

--- src/Dress/Test.php
+++ src/Dress/Test.php
@@ -3,6 +3,9 @@ namespace Gajus\Dora\Dress;
 
 /**
  * This dress is used for unit testing.
+ * @todo Move to tests.
+ * @link https://github.com/gajus/dora for the canonical source repository
+ * @license https://github.com/gajus/dora/blob/master/LICENSE BSD 3-Clause
  */
 class Test extends Manikin {
 	public function toString () {
--- src/Form.php
+++ src/Form.php
@@ -1,6 +1,10 @@
 <?php
 namespace Gajus\Dora;
 
+/**
+ * @link https://github.com/gajus/dora for the canonical source repository
+ * @license https://github.com/gajus/dora/blob/master/LICENSE BSD 3-Clause
+ */
 class Form {
 	private
 		/**
