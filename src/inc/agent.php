edf87ea51e0131f605c250a2963d140c80abf303
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-03-25 12:43:46 +0000

    No prejudice to the method used to submit the form.

--- src/inc/agent.php
+++ src/inc/agent.php
@@ -8,10 +8,11 @@ register_shutdown_function(function () {
         throw new \LogicException('Session must be started before using Dora.');
     }
 
-    // Make a copy of the post data.
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$_SESSION['gajus']['dora']['flash'] = $_POST;
-
+    // Make a copy of the input submitted using Dora form.
+    if (isset($_POST['gajus']['dora']['uid'])) {
+        $_SESSION['gajus']['dora']['flash'] = $_POST;
+    } else if (isset($_GET['gajus']['dora']['uid'])) {
+        $_SESSION['gajus']['dora']['flash'] = $_GET;
 	// Delete copy on the first page that does not issue a redirect.
 	} else if (!array_filter(headers_list(), function ($e) { return strpos($e, 'Location:'); })) {
 		unset($_SESSION['gajus']['dora']['flash']);
