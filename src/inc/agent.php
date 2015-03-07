1b0945ccd73d7b448e5de782ad9ab7032dcf782c
Gajus Kuizinas <gk@anuary.com>
2015-03-07 14:15:19 +0000

    As of PHP 5.4, reading raw HTTP input requires an extra php.ini parameter. Furthermore, this method of obtaining input copy is more efficient.

--- src/inc/agent.php
+++ src/inc/agent.php
@@ -3,17 +3,17 @@ if (php_sapi_name() === 'cli') {
     return;
 }
 
-register_shutdown_function(function () {
+// Make a copy of $_POST before the main script could be have interfered with it.
+$input = $_POST;
+
+register_shutdown_function(function () use ($input) {
     if (session_status() === \PHP_SESSION_NONE) {
         throw new \LogicException('Session must be started before using Dora.');
     }
 
     // Make a copy of the input submitted using Dora form.
-    if (isset($_POST['gajus']['dora']['uid'])) {
-        // It might be that $_POST have been modified during the execution of the script.
-        parse_str(file_get_contents('php://input'), $input);
-
-        $_SESSION['gajus']['dora']['flash'][$_POST['gajus']['dora']['uid']] = $input;
+    if (isset($input['gajus']['dora']['uid'])) {
+        $_SESSION['gajus']['dora']['flash'][$input['gajus']['dora']['uid']] = $input;
     } else if (!array_filter(headers_list(), function ($e) { return mb_strpos(mb_strtolower($e), 'location:') !== false; })) {
         unset($_SESSION['gajus']['dora']['flash']);
     }
